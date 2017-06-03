5b54592f180e21c4d0b506958dc5d89db5179fcf
qin chen <1837830365@qq.com>
2017-06-03 22:35:45 +0800

    2333

--- testopencv/main.cpp
+++ testopencv/main.cpp
@@ -4,19 +4,15 @@
 #include <string> 
 
 using namespace cv;
-int main()
+
+int main(void)
 {
 
 	Mat img = imread("pic.jpg", CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
 
 	namedWindow("游戏原画");
-
 	imshow("游戏原画",img);
-
 	waitKey(6000);
-
 	return 0;
-
-
 
 }
